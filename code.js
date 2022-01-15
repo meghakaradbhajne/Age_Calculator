7f4b28d633e8b84240135edcf46b00d41ea3d65e
Megha <meghakaradbhajne21@gmail.com>
2022-01-15 16:06:10 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,107 @@
+//calculate age
+let daysH=document.getElementById("date");
+let monthH=document.getElementById("month");
+let yearH=document.getElementById("year");
+
+let showAge=document.getElementById("show-age");
+
+const btn=document.getElementById("calc-btn");
+
+var today=new Date();
+
+let d=today.getDate();
+let m=today.getMonth()+1;
+let y=today.getFullYear();
+
+let maxDays=0;
+let monthNum=1;
+
+btn.addEventListener("click",( days, month,year)=>{
+    days=daysH.value;
+    month=monthH.value;
+    year=yearH.value;
+
+    if(daysH.value==""|| monthH.value==""||yearH.value==""){
+        return alert("please enter your Date of Birth properly to calculate your age");
+    } 
+
+console.log('Current Date:${d} ${m} ${y}');
+
+if(m==1){
+    maxDays=31;
+    monthNum=1;
+}
+else if(m==2){
+
+    maxDays=28;
+    monthNum=2;
+}
+else if (m==3){
+    maxDays=31;
+    monthNum=3;
+}
+
+else if(m==4){
+    
+    maxDays=30;
+    monthNum=4;
+}
+
+else if(m==5){
+    
+    maxDays=31;
+    monthNum=5;
+}
+
+else if(m==6){
+    
+    maxDays=30;
+    monthNum=6;
+}
+
+else if(m==7){
+    
+    maxDays=31;
+    monthNum=7;
+}
+
+else if(m==8){
+    
+    maxDays=31;
+    monthNum=8;
+}
+
+else if(m==9){
+    
+    maxDays=30;
+    monthNum=9;
+}
+
+else if(m==10){
+    maxDays=31;
+    monthNum=10;
+}
+    
+else if(m==11){
+    
+    maxDays=30;
+    monthNum=11;
+}
+
+else{
+    
+    maxDays=31;
+    monthNum=12;
+}
+
+var ageYear=y-year;
+if(m<=month){
+    m=m+12;
+}
+var ageMonth=m-month;
+
+var ageDays=(maxDays-d)+ parseInt(days);
+
+console.log('Your age is ${ageYears}y,${ageMonth} m and ${ageDays} d');
+showAge.InnerHTMl='Your age is ${ageYears}y,${ageMonth} m and ${ageDays}d';
+})
